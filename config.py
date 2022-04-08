17846a3b42e21327211fabccda3406baf0698ad5
zlopez101 <zachlopez9@gmail.com>
2022-04-08 13:33:15 -0500

    updated requirements

--- config.py
+++ config.py
@@ -4,7 +4,6 @@
 
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 """ Bot Configuration """
 
--- insurance_checker/async_api.py
+++ insurance_checker/async_api.py
@@ -1,11 +1,15 @@
 """Make Asynchronous API calls to the Airtable API
 """
-from audioop import mul
 from typing import List
-from webbrowser import get
-from aiohttp import ClientSession, ClientResponse, ClientResponseError
+from aiohttp import ClientSession, ClientResponse
 from .models import Insurance, CPT_code, Provider
-from .utils import exact_value_filter, select_fields, filter_unique, multiple_exact_value_filter
+from .utils import (
+    exact_value_filter,
+    select_fields,
+    filter_unique,
+    multiple_exact_value_filter,
+)
+
 from dotenv import load_dotenv
 from enum import Enum
 import os
@@ -16,6 +20,7 @@ HEADERS = {
     "Authorization": f"Bearer {os.environ['API_key']}",
     "Content-Type": "application/json",
 }
+
 
 class URL(Enum):
 
@@ -29,6 +34,7 @@ INS_URL = f"https://api.airtable.com/v0/{os.environ['airtable_base_id']}/{os.env
 CPT_URL = f"https://api.airtable.com/v0/{os.environ['airtable_base_id']}/{os.environ['cpt_codes_table_id']}"
 FC_URL = f"https://api.airtable.com/v0/{os.environ['airtable_base_id']}/{os.environ['fc_table_id']}"
 PROVIDER_URL = f"https://api.airtable.com/v0/{os.environ['airtable_base_id']}/{os.environ['providers_table_id']}"
+
 
 class Session:
     def __init__(self):
@@ -46,27 +52,31 @@ class Session:
 
 async def _raise_for_status(response: ClientResponse, response_object):
 
-        response.raise_for_status()
-        resp: dict = await response.json()
-        offset = resp.pop('offset', "")
-        records = resp.pop('records', {})
-        # if retrieving a single record, the response object doesn't contain `key` "records"
-        if records:
-            res = [response_object.from_api(record) for record in records]
-        else:
-            res = response_object.from_api(resp)
-        return res, offset
+    response.raise_for_status()
+    resp: dict = await response.json()
+    offset = resp.pop("offset", "")
+    records = resp.pop("records", {})
+    # if retrieving a single record, the response object doesn't contain `key` "records"
+    if records:
+        res = [response_object.from_api(record) for record in records]
+    else:
+        res = response_object.from_api(resp)
+    return res, offset
+
 
 async def _list_records(session: ClientSession, url: URL, params: str = None):
-    
+
     async with session.get(url):
         pass
+
 
 async def _retrieve_record(session: ClientSession, url: URL, id: str):
     pass
 
+
 async def _create_record(session: ClientSession, url: URL, data):
     pass
+
 
 async def get_payers(session: ClientSession) -> list:
     params = (
@@ -79,20 +89,28 @@ async def get_payers(session: ClientSession) -> list:
 
 
 async def get_coverages_by_payer_name(
-    session: ClientSession, payerName: str, inn_only: bool=True, pageSize: int = 5, offset: str = None
+    session: ClientSession,
+    payerName: str,
+    inn_only: bool = True,
+    pageSize: int = 5,
+    offset: str = None,
 ) -> List[Insurance]:
-    
+
     params = dict(pageSize=pageSize)
     if offset:
-        params['offset'] = offset
-        
+        params["offset"] = offset
+
     if inn_only:
-        params.update(multiple_exact_value_filter(("payer_name", payerName), ("network_status", "INN")))
+        params.update(
+            multiple_exact_value_filter(
+                ("payer_name", payerName), ("network_status", "INN")
+            )
+        )
     else:
         params.update(exact_value_filter("payer_name", payerName))
 
     async with session.get(INS_URL, params=params) as resp:
-            return await _raise_for_status(resp, Insurance)
+        return await _raise_for_status(resp, Insurance)
 
 
 async def get_coverage_by_id(session: ClientSession, id: str) -> Insurance:
@@ -139,6 +157,7 @@ async def get_financial_classes_by_name(session: ClientSession, name: str) -> di
     # async with session.get(FC_URL, params=exact_value_filter('name', name)) as resp:
     # r
 
+
 async def get_locations(session: ClientSession) -> List[str]:
     """Get the CBC locations
 
@@ -150,12 +169,18 @@ async def get_locations(session: ClientSession) -> List[str]:
     """
     async with session.get(PROVIDER_URL) as resp:
         results = await resp.json()
-        records = results['records']
-        return list(set([record['fields']['Location'] for record in records]))
+        records = results["records"]
+        return list(set([record["fields"]["Location"] for record in records]))
 
-async def get_providers_at_location(session: ClientSession, location: str) -> List[Provider]:
-    async with session.get(PROVIDER_URL, params=exact_value_filter('Location', location)) as resp:
+
+async def get_providers_at_location(
+    session: ClientSession, location: str
+) -> List[Provider]:
+    async with session.get(
+        PROVIDER_URL, params=exact_value_filter("Location", location)
+    ) as resp:
         return await _raise_for_status(resp, Provider)
+
 
 if __name__ == "__main__":
     import asyncio
@@ -168,12 +193,14 @@ if __name__ == "__main__":
 
             print(page[0])
 
-
-            next_page, offset = await get_coverages_by_payer_name(session, "BCBSTX", offset=offset)
+            next_page, offset = await get_coverages_by_payer_name(
+                session, "BCBSTX", offset=offset
+            )
 
             print(next_page[0])
 
             assert page != next_page
+
     asyncio.set_event_loop_policy(
         asyncio.WindowsSelectorEventLoopPolicy()
     )  # windows issue
